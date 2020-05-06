87a929d857ba09d7f4a0dd4c3cbdcbf7bc469418
travischantravis <chancheukyin4463@gmail.com>
2020-05-05 22:20:37 -0700

    initial set up

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// connect to MongoDB
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+// Listen to port
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
